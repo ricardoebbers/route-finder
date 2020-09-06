f70626a42cda8f1e35531e6ccd544950836652e3
Ricardo Ebbers <recl@cin.ufpe.br>
2020-09-06 17:25:17 -0300

    Updated the Main to use the adapter and solve the challenge

--- src/main/java/routefinder/Main.java
+++ src/main/java/routefinder/Main.java
@@ -1,9 +1,29 @@
 package routefinder;
 
+import routefinder.adapter.FileIOAdapter;
+import routefinder.adapter.impl.StringToFileIOAdapter;
+import routefinder.domain.entity.City;
+import routefinder.domain.entity.Graph;
+import routefinder.domain.entity.Scorer;
+import routefinder.domain.repository.DistanceRepository;
+import routefinder.domain.repository.impl.DistanceRepositoryImpl;
+import routefinder.domain.usecase.ShortestPath;
+import routefinder.domain.usecase.astar.DistanceGraph;
+import routefinder.domain.usecase.astar.DistanceScorer;
+import routefinder.domain.usecase.astar.RouteFinder;
+
 public class Main {
 
+    private static final DistanceRepository<City> distanceRepository = new DistanceRepositoryImpl<>();
+    private static final Graph<City> graph = new DistanceGraph<>(distanceRepository);
+    private static final Scorer<City> scorer = new DistanceScorer<>(distanceRepository);
+    private static final ShortestPath<City> shortestPath = new RouteFinder<>(graph, scorer);
+    private static final FileIOAdapter fileIOAdapter = new StringToFileIOAdapter(shortestPath);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        fileIOAdapter.loadDistances("trechos.txt");
+        String result = fileIOAdapter.calculateDistances("encomendas.txt");
+        fileIOAdapter.writeToOutput("rotas.txt", result);
     }
 
 }
